7cfe0ad6b336e2f7dfb40b1d27dabfc5ee910d58
Jaev <christiantejano18@gmail.com>
2026-01-04 21:19:41 +0800

    Fixed failing tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
 test('registration screen can be rendered', function () {
     $response = $this->get(route('register'));
 
@@ -7,15 +10,21 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+    Storage::fake('public');
+
     $response = $this->post(route('register.store'), [
         'name' => 'John Doe',
+        'designation' => 'Test Designation',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
+        'e_signature' => UploadedFile::fake()->image('signature.jpg'),
     ]);
 
     $response->assertSessionHasNoErrors()
         ->assertRedirect(route('dashboard', absolute: false));
 
     $this->assertAuthenticated();
+
+    Storage::disk('public')->assertExists('signatures/' . basename(auth()->user()->e_signature));
 });
